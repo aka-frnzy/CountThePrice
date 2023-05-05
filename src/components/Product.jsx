7c50b0d3f86f9ae29f673458b2ce6f2b410b2ba3
frnzy <dhakalsuraj950@gmail.com>
2023-05-05 20:47:56 +0545

    Added variables so can use for multiple products

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-const Product = ({ price }) => {
+const Product = ({ name, price, product }) => {
     const [count, setCount] = useState(0);
     const add = () => {
         setCount((count) => count + 1);
@@ -11,8 +11,8 @@ const Product = ({ price }) => {
         <>
             <div class="product-list">
                 <div class="product-item">
-                    <img src="https://res.cloudinary.com/teepublic/image/private/s--T4BhhRF3--/t_Resized%20Artwork/c_crop,x_10,y_10/c_fit,w_470/c_crop,g_north_west,h_626,w_470,x_0,y_0/g_north_west,u_upload:v1462829015:production:blanks:mtl53ofohwq5goqjo9ke,x_-395,y_-325/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1488815300/production/designs/1294242_1.jpg" height={400} alt="Product" />
-                    <h3>T-shirt</h3>
+                    <img src={product} height={400} alt="Product" />
+                    <h3>{name}</h3>
                     <p>Price:  Only  ${price}</p>
                     <div class="quantity">
                         <button onClick={minus} >-</button>
